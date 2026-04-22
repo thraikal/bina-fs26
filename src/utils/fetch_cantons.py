d801eeda86575fa59ee3a8a6516a25db06ac3e75
Marc Kalbermatter <ma.kalbermatter@gmail.com>
2026-04-22 19:58:39 +0200

    - Add premius - Fix ssl error on mac

--- src/utils/fetch_cantons.py
+++ src/utils/fetch_cantons.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import geopandas as gpd
 import requests
+import certifi
 
 # URL copied from https://www.swisstopo.admin.ch/en/landscape-model-swissboundaries3d
 URL = "https://data.geo.admin.ch/ch.swisstopo.swissboundaries3d/swissboundaries3d_2026-01/swissboundaries3d_2026-01_2056_5728.gpkg.zip"
@@ -22,7 +23,7 @@ def load_cantons_geojson(data_dir: Path) -> dict:
 
 def _download_zip() -> bytes:
     print("Downloading swissBOUNDARIES3D...")
-    response = requests.get(URL, timeout=60)
+    response = requests.get(URL, timeout=60, verify=certifi.where())
     response.raise_for_status()
     return response.content
 
